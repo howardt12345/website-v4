d5d57947b2a9c1ba73c571806547d9e082274569
Howard Tseng <howardt12345@gmail.com>
2023-06-03 02:13:44 -0400

    Revert "cleanup font"
    This reverts commit 683e7c7cd2b4d5f5434262963c565c7247e6186c.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import MainView from '@/views/main-view.vue'
+import MainView from '@/views/main-view.vue';
 </script>
 
 <template>
   <MainView />
 </template>
 
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins@1&display=swap');
+<style scoped>
+
 </style>
