c764e88498375144bea12c399c0779040c23c3f4
Howard Tseng <howardt12345@gmail.com>
2023-08-30 16:48:31 -0400

    vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import './assets/sass/style.scss'
-import PoppinsRegular from './assets/fonts/Poppins-Regular.ttf'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -7,9 +6,22 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(vuetify)
 
 app.mount('#app')
