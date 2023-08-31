96eff467f5b269aec2a3425ccf1cc5ad1eaf3a36
Howard Tseng <howardt12345@gmail.com>
2023-08-30 22:40:59 -0400

    added vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  rootDir: "src/",
-  css: ["@/assets/scss/global.scss"],
+  rootDir: 'src/',
+  css: ['@/assets/scss/global.scss'],
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@use "@/assets/scss/_vars.scss" as *;',
-        },
-      },
-    },
+          additionalData: '@use "@/assets/scss/_vars.scss" as *;'
+        }
+      }
+    }
   }
-});
+})
