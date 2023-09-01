39797338172bf790fb879cf7eab69cdd7c595ce7
Howard Tseng <howardt12345@gmail.com>
2023-08-31 23:37:46 -0400

    fix: theme switching

--- src/store/theme.store.ts
+++ src/store/theme.store.ts
@@ -1,12 +1,11 @@
 import { defineStore } from 'pinia';
 import { useTheme } from 'vuetify';
-import { computed } from 'vue';
 
 export const useThemeStore = defineStore('theme', () => {
   const theme = useTheme();
   const isDark = computed(() => theme.global.current.value.dark);
   const toggleTheme = () => {
-    isDark.value ? 'light' : 'dark';
+    theme.global.name.value = isDark.value ? 'light' : 'dark';
   };
 
   return {
