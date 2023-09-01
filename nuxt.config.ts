78ce1236d10dbb4f88cf7230c6431b2b343405e5
Howard Tseng <howardt12345@gmail.com>
2023-08-31 21:31:45 -0400

    theme colors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,47 @@
+const ACCENT = '#2196F3'
+const ACCENT_dark = '#64ffda'
+const DARK_BG = '#EAEAEA'
+const DARK_BG_dark = '#202020'
+const BG = '#FFFFFF'
+const BG_dark = '#121212'
+
+const theme_light = {
+  dark: false,
+  colors: {
+    background: BG,
+    surface: BG,
+    'surface-variant': DARK_BG,
+    primary: ACCENT,
+    textPrimary: '#000000',
+    textSecondary: '#7A7A7A',
+    textBody: '#0C0C0C',
+    translucent_accent: 'rgba(33, 150, 243, 0.07)',
+    shadow_bg: 'rgba(234, 234, 234, 0.07)',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00'
+  }
+}
+
+const theme_dark = {
+  dark: true,
+  colors: {
+    background: BG_dark,
+    background_secondary: DARK_BG_dark,
+    primary: ACCENT_dark,
+    'text-primary': '#FFFFFF',
+    'text-secondary': '#858585',
+    'text-body': '#F3F3F3',
+    'translucent-accent': 'rgba(100, 255, 218, 0.07)',
+    shadow_bg: 'rgba(21, 21, 21, 0.07)',
+    error: '#CF6679',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00'
+  }
+}
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
@@ -16,83 +60,12 @@ export default defineNuxtConfig({
   vuetify: {
     vuetifyOptions: {
       theme: {
-        defaultTheme: 'light',
         options: {
-            customProperties: true,
+          customProperties: true
         },
         themes: {
-          light: {
-            colors: {
-              background: '#FFFFFF',
-              surface: '#FFFFFF',
-              'surface-variant': '#424242',
-              'on-surface-variant': '#EEEEEE',
-              primary: '#6200EE',
-              'primary-darken-1': '#3700B3',
-              secondary: '#03DAC6',
-              'secondary-darken-1': '#018786',
-              error: '#B00020',
-              info: '#2196F3',
-              success: '#4CAF50',
-              warning: '#FB8C00'
-            },
-            dark: false,
-            variables: {
-              'border-color': '#000000',
-              'border-opacity': 0.12,
-              'high-emphasis-opacity': 0.87,
-              'medium-emphasis-opacity': 0.6,
-              'disabled-opacity': 0.38,
-              'idle-opacity': 0.04,
-              'hover-opacity': 0.04,
-              'focus-opacity': 0.12,
-              'selected-opacity': 0.08,
-              'activated-opacity': 0.12,
-              'pressed-opacity': 0.12,
-              'dragged-opacity': 0.08,
-              'theme-kbd': '#212529',
-              'theme-on-kbd': '#FFFFFF',
-              'theme-code': '#F5F5F5',
-              'theme-on-code': '#000000',
-              'my-color-value': '#81df3a'
-            }
-          },
-          dark: {
-            colors: {
-              background: '#121212',
-              surface: '#212121',
-              'surface-variant': '#BDBDBD',
-              'on-surface-variant': '#424242',
-              primary: '#BB86FC',
-              'primary-darken-1': '#3700B3',
-              secondary: '#03DAC5',
-              'secondary-darken-1': '#03DAC5',
-              error: '#CF6679',
-              info: '#2196F3',
-              success: '#4CAF50',
-              warning: '#FB8C00'
-            },
-            dark: true,
-            variables: {
-              'border-color': '#FFFFFF',
-              'border-opacity': 0.12,
-              'high-emphasis-opacity': 1,
-              'medium-emphasis-opacity': 0.7,
-              'disabled-opacity': 0.5,
-              'idle-opacity': 0.1,
-              'hover-opacity': 0.04,
-              'focus-opacity': 0.12,
-              'selected-opacity': 0.08,
-              'activated-opacity': 0.12,
-              'pressed-opacity': 0.16,
-              'dragged-opacity': 0.08,
-              'theme-kbd': '#212529',
-              'theme-on-kbd': '#FFFFFF',
-              'theme-code': '#343434',
-              'theme-on-code': '#CCCCCC',
-              'my-color-value': '#81df3a'
-            }
-          }
+          light: theme_light,
+          dark: theme_dark
         }
       }
     }
