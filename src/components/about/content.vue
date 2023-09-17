d5ab4146491ffe0040a45c9adda91b08543e2986
Howard Tseng <howardt12345@gmail.com>
2023-09-17 02:09:19 -0400

    put about in content

---
+++ src/components/about/content.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+interface Props {
+  data: ParsedContent;
+}
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <h1 class="section-title">{{ $t('About') }}</h1>
+  <div class="about">
+    <div class="about__content">
+      <ContentRenderer class="content-renderer" :value="data" />
+      <v-btn
+        href="/resume.pdf"
+        target="_blank"
+        class="resume-button"
+        size="large"
+        color="primary"
+        >{{ $t('Resume') }}</v-btn
+      >
+      <AboutSkills
+        :technologies="data.technologies"
+        :hobbies="data.hobbies"
+        :languages="data.languages"
+      />
+    </div>
+    <div class="about__image">image</div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.about {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  margin-bottom: rem(32);
+
+  &__content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  &__image {
+    width: 100%;
+    max-width: 400px;
+    margin-bottom: rem(16);
+    border: 1px solid $border-color;
+  }
+
+  @media (max-width: 600px) {
+    flex-direction: column;
+    margin-bottom: rem(16);
+
+    &__image {
+      max-width: 100%;
+    }
+  }
+}
+
+.content-renderer {
+  :deep(p) {
+    margin-bottom: rem(8);
+  }
+  :last-child {
+    margin-bottom: 0;
+  }
+}
+
+.resume-button {
+  margin: rem(24);
+}
+</style>
