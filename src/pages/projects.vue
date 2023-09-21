3862e6ff3d5d95426aea52eab77cdcbeb21d8501
Howard Tseng <howardt12345@gmail.com>
2023-09-21 11:36:50 -0400

    refractor: using vueuse array

--- src/pages/projects.vue
+++ src/pages/projects.vue
@@ -1,17 +1,26 @@
 <script setup lang="ts">
+import { useArrayFilter } from '@vueuse/core';
 import { ProjectItem } from '@/types/projects';
 
-const { data: projects } = await useAsyncData('projects', () =>
-  queryContent<ProjectItem>('/projects').sort({ date: -1 }).find(),
+const { data: projects } = await useAsyncData(
+  'projects',
+  () => queryContent<ProjectItem>('/projects').sort({ date: -1 }).find(),
+  { default: () => [] as ProjectItem[] },
+);
+const { data: tags } = await useAsyncData('projects-tags', () =>
+  queryContent<string[]>('/projects').only(['tech']).find(),
 );
 
-const featuredProjects = computed<ProjectItem[]>(
-  () => projects?.value?.filter((project) => project.featured) ?? [],
+const featuredProjects = useArrayFilter(
+  projects,
+  (project: ProjectItem) => project.featured ?? false,
 );
 
-const otherProjects = computed<ProjectItem[]>(
-  () => projects?.value?.filter((project) => !project.featured) ?? [],
+const otherProjects = useArrayFilter(
+  projects,
+  (project: ProjectItem) => !project.featured ?? true,
 );
+
 </script>
 
 <template>
