fe4988563a9f4c94f3f6fa23c6add35fa4e0502a
Howard Tseng <howardt12345@gmail.com>
2023-09-21 14:22:40 -0400

    wip: project filter by tag

---
+++ src/components/projects/filtered.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { useVModel } from '@vueuse/core';
+
+interface Props {
+  tags: string[];
+  selectedTags: string[];
+}
+const props = defineProps<Props>();
+const emits = defineEmits<{
+  (event: 'update:selectedTags', value: string[]): void;
+}>();
+
+const selectedTags = useVModel(props, 'selectedTags', emits);
+const selectedIndexes = ref<number[]>([]);
+
+watch(selectedIndexes, (newValue) => {
+  selectedTags.value = newValue.map((index) => props.tags[index]);
+})
+</script>
+
+<template>
+  <v-chip-group
+    v-model="selectedIndexes"
+    filter
+    multiple
+    color="primary"
+  >
+    <v-chip v-for="tag in tags" filter>{{ tag }}</v-chip>
+  </v-chip-group>
+</template>
--- src/pages/projects.vue
+++ src/pages/projects.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
 import { useArrayFilter } from '@vueuse/core';
 import { ProjectItem } from '@/types/projects';
+import { compile } from 'nuxt/dist/app/compat/capi';
+
+interface TechOnly {
+  tech: string[];
+}
 
 const { data: projects } = await useAsyncData(
   'projects',
   () => queryContent<ProjectItem>('/projects').sort({ date: -1 }).find(),
   { default: () => [] as ProjectItem[] },
 );
-const { data: tags } = await useAsyncData('projects-tags', () =>
-  queryContent<string[]>('/projects').only(['tech']).find(),
+const { data: tags } = await useAsyncData(
+  'projects-tags',
+  () => queryContent<TechOnly>('/projects').only('tech').find(),
+  {
+    default: () => [] as TechOnly[],
+  },
 );
 
 const featuredProjects = useArrayFilter(
@@ -21,10 +30,17 @@ const otherProjects = useArrayFilter(
   (project: ProjectItem) => !project.featured ?? true,
 );
 
+const uniqueTags = computed<Set<string>>(
+  () => new Set(tags.value.map((tag) => tag.tech).flat()),
+);
+
+const selectedTags = ref<string[]>([]);
 </script>
 
 <template>
   <h1 class="section-title">{{ $t('Projects') }}</h1>
+  <!-- <ProjectsFiltered :tags="Array.from(uniqueTags)" v-model:selected-tags="selectedTags" />
+  <div>{{ selectedTags }}</div> -->
   <ProjectsFeatured :projects="featuredProjects" />
   <ProjectsOther :projects="otherProjects" />
 </template>
