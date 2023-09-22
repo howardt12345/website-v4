df8f553362cf9cc57774aeda2a074b879579a6a0
Howard Tseng <howardt12345@gmail.com>
2023-09-22 17:43:57 -0400

    padding changes

--- src/pages/photography/index.vue
+++ src/pages/photography/index.vue
@@ -2,26 +2,23 @@
 import { usePhotosStore } from '@/store/photos.store';
 
 const { photos } = storeToRefs(usePhotosStore());
-
 </script>
 
 <template>
   <h1 class="section-title">Photos</h1>
-  <v-container>
-    <v-row v-masonry no-gutters>
-      <v-col
-        v-for="(photo, index) in photos"
-        v-masonry-tile
-        :key="`photo-${index}`"
-        cols="12"
-        sm="12"
-        md="6"
-        lg="4"
-      >
-        <PhotosCard :photo="photo" />
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-row v-masonry dense>
+    <v-col
+      v-for="(photo, index) in photos"
+      v-masonry-tile
+      :key="`photo-${index}`"
+      cols="12"
+      sm="12"
+      md="6"
+      lg="4"
+    >
+      <PhotosCard :photo="photo" />
+    </v-col>
+  </v-row>
 </template>
 
 <style scoped lang="scss"></style>
