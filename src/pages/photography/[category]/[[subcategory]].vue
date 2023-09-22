aed29e16dd4a793076b8f42146170811dd543ff8
Howard Tseng <howardt12345@gmail.com>
2023-09-21 21:36:30 -0400

    init breadcrumbs

--- src/pages/photography/[category]/[[subcategory]].vue
+++ src/pages/photography/[category]/[[subcategory]].vue
@@ -1,10 +1,31 @@
+<script setup lang="ts">
+const route = useRoute();
+
+const category = computed(() => route.params.category);
+const subcategory = computed(() => route.params.subcategory);
+
+const breadcrumbItems = computed(() => {
+  const items = [
+    {
+      title: 'Photos',
+      href: '/photography',
+    },
+    {
+      title: category.value,
+      href: `/photography/${category.value}`,
+    },
+  ];
+  if (subcategory.value) {
+    items.push({
+      title: subcategory.value,
+      href: `/photography/${category.value}/${subcategory.value}`,
+    });
+  }
+  return items;
+});
+</script>
+
 <template>
   <h1 class="section-title">Photos - Category</h1>
-  <v-breadcrumbs
-    :items="[
-      'Photos',
-      $route.params.category,
-      ...($route.params.subcategory ? [$route.params.subcategory] : []),
-    ]"
-  ></v-breadcrumbs>
+  <v-breadcrumbs :items="breadcrumbItems"></v-breadcrumbs>
 </template>
