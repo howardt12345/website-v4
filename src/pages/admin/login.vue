523fa32d12a452cd11f999191c144e4c8b2aeb07
Howard Tseng <howardt12345@gmail.com>
2023-09-26 15:14:06 -0400

    feat: init admin page

---
+++ src/pages/admin/login.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useAdminStore } from '@/store/admin.store';
+
+const adminStore = useAdminStore();
+const { signIn } = adminStore;
+
+const email = ref<string>('');
+</script>
+
+<template>
+  <h1>login</h1>
+  <input v-model="email" />
+  <v-btn @click="signIn">Log in</v-btn>
+</template>
+
+<style scoped lang="scss"></style>
--- src/store/admin.store.ts
+++ src/store/admin.store.ts
@@ -1,5 +1,23 @@
+const PATH = '/admin';
 
 export const useAdminStore = defineStore('admin', () => {
+  const supabase = useSupabaseClient();
+  const user = useSupabaseUser();
+  const hasUser = computed<boolean>(() => !!user.value);
 
-  
+  const signIn = async (email: string) => {
+    const { error } = await supabase.auth.signInWithOtp({
+      email,
+      options: {
+        emailRedirectTo: process.env.WEBSITE_URL + PATH,
+      },
+    });
+    if (error) console.log(error);
+  };
+
+  return {
+    user,
+    hasUser,
+    signIn
+  };
 });
