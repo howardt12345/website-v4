1903f7de44a5a3603dbf26ce1898fb454ed5ed74
Howard Tseng <howardt12345@gmail.com>
2023-09-27 00:58:39 -0400

    make all tags lowercase

--- src/store/photos.store.ts
+++ src/store/photos.store.ts
@@ -12,6 +12,10 @@ export const usePhotosStore = defineStore('photos', () => {
       .select();
     photos.value = <PhotoItem[]>photosData;
     categories.value = <PhotoCategory[]>categoriesData;
+
+    photos.value.forEach((photo) => {
+      photo.tags = photo.tags.map((tag) => tag.toLowerCase());
+    });
   };
 
   return {
