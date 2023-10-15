90a52df08667cc45e34fe037ccbd75ff5c849d30
Howard Tseng <howardt12345@gmail.com>
2023-10-14 20:06:55 -0400

    email button

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,9 +1,14 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <h1 class="section-title">Contact</h1>
+  <v-btn
+    href="mailto:me@howardt12345.com"
+    target="_blank"
+    rel="nofollow noopener noreferrer"
+  >
+    Email - me@howardt12345.com
+  </v-btn>
 </template>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
